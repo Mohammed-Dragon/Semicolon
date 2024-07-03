d9a797b59ba6dbfd848214709d5ffe66ad5e2876
Mohammed-Dragon <alu3535dragon@gmail.com>
2024-07-03 21:35:55 +0300

    my first commit

---
+++ semicolon.dart
@@ -0,0 +1,8 @@
+/*
+what is version control?
+Version control is a system that manages changes to a set of files over time.
+what is git?
+Git is a distributed version control system.
+what is github, gitlab?
+GitHub is a web-based platform that uses Git for version control.
+ */
